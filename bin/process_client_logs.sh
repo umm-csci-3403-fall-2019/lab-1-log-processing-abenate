df615b8b188236c749551ba21e4b06594b81e190
nkfoss <nathankfoss@gmail.com>
2019-09-10 18:39:01 -0500

    Finalize process_client_logs and modify process_logs for new directory

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,6 +1,2 @@
+awk 'match($0, /(.+ [0-9]{2}):.* Failed .* (for|user) (.*) from (.*) port/, groups) {print groups[1] " " groups[3] " " groups[4] }' $1/var/log/* >> $1/failed_login_data.txt
 
-cd $1/var/log
-
-cat * > bigFile.txt
-
-awk 'match($0, /(.+ [0-9]{2}):.* (for|user) (.*) from (.*) port/, groups) {print groups[1] " " groups[3] " " groups[4] }' bigFile.txt > failed_login_data.txt
--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 here=$(pwd)
 
-mkdir $here/scratch
+mkdir scratch
 
-for VARIABLE in $1 $2 $3 $4 $5 $6
+for VARIABLE in $@;
 do
-	cd $here/scratch
-	dirString=${VARIABLE/.tgz}
-	mkdir $dirString
+	#cd $here/scratch
+	dirString=${VARIABLE/_"secure.tgz"}
+	mkdir $here/scratch/$dirString
 
-	cd $here/log_files
-	tar -zxf "$VARIABLE" -C $here/scratch/$dirString
+	#cd $here/log_files
+	tar -zxf "./log_files/$VARIABLE" -C $here/scratch/$dirString
 	
     #current=$"($here/scratch/$dirString/var/log)"
 
-	$here/bin/process_client_logs.sh $here/scratch/$dirString
+	bin/process_client_logs.sh $here/scratch/$dirString
 
 done
+
+# cd $here/scratch
+
+# cat cscirepo_secure/var/log/failed_login_data.txt >> failed_login_data.txt 
+# cat ganesha_secure/var/log/failed_login_data.txt >> failed_login_data.txt 
+# cat velcro_secure/var/log/failed_login_data.txt >> failed_login_data.txt 
+# cat discovery_secure/var/log/failed_login_data.txt >> failed_login_data.txt 
+# cat mylar_secure/var/log/failed_login_data.txt >> failed_login_data.txt 
+# cat zeus_secure/var/log/failed_login_data.txt >> failed_login_data.txt 
+
 
 
 
