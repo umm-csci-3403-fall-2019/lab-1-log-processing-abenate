f70ed6e8efd21181c6a4254a61d4ddc4526b5687
monjo003 <monjo003@morris.umn.edu>
2019-09-15 14:28:50 -0500

    we sorted the IPs and we are working to get userCountry_body.txtdone

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -9,3 +9,12 @@ done
 
 
 awk 'match($0, /.* ([0-9\.]*)/, groups) {print groups[1]}' $1/all_country_failed_login.txt| sort | uniq -c > $1/outFileCountry.txt 
+
+awk '{ t=$1 ; $1=$2; $2=t; print }' $1/outFileCountry.txt > $1/temp1.txt
+awk '{ t=$1 ; $1=$2; $2=t; print }' etc/country_IP_map.txt > $1/temp2.txt
+
+awk '{ t=$1 ; $1=$2; $2=t; print }' etc/country_IP_map.txt > $1/temp_final.txt
+
+
+awk 'match($0, /([0-9]) ([A-Z]{2})+\\$/, groups) {print "data.addRow([\x27"groups[2]"\x27, " groups[1]"]);"}' $1/temp_final.txt > $1/userCountry_body.txt
+
